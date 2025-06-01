170e1513897aac9e7050681681669ff894cf2fce
rouhanaom45 <200379123+rouhanaom45@users.noreply.github.com>
2025-06-02 00:35:41 +0200

    Update play.sh

--- play.sh
+++ play.sh
@@ -9,6 +9,15 @@ else
     exit 1
 fi
 
+# Run open.sh using bash
+if [ -f open.sh ]; then
+    echo "Running open.sh..."
+    bash open.sh
+else
+    echo "Error: open.sh not found."
+    exit 1
+fi
+sleep 2
 # Run start.sh after setupo.sh completes
 if [ -f start.sh ]; then
     echo "Running start.sh..."
